d4e37e294f08c3e3e8ad5fcb8ed9b9e92acdbad2
N Diaye C Abdul <abdul13un@gmail.com>
2025-07-21 13:08:15 +0200

    Désactivation régle esLint pour mise à jour React moderne + suppression defaultProps

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     "react/jsx-props-no-spreading": 'off',
     "react/function-component-definition": 'off',
+    'react/require-default-props': 'off',
     "no-console": 'off'
   },
 };
